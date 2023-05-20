142ea26efb44603af46c2412b0ce09d47f49ac15
Dominic Muli <118108559+Dominic-Mu@users.noreply.github.com>
2023-05-20 14:55:11 +0300

    Add Python file

---
+++ coolapp.py
@@ -0,0 +1,5 @@
+my_goal = "Learn Data Science and Programming"
+class_goal = "Make the world a better place"
+
+print(f"I would like to {my_goal}, so that I can {class_goal}!", end=" ")
+print("It feels so cool to be able to do this!!", end="\n")
