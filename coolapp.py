99fa22e3940f1866754eee30a4356f906c245b9d
Dominic Muli <118108559+Dominic-Mu@users.noreply.github.com>
2023-05-22 21:03:03 +0300

    added for loops

--- coolapp.py
+++ coolapp.py
@@ -1,5 +1,17 @@
 my_goal = "Learn Data Science and Programming"
 class_goal = "Make the world a better place"
-
 print(f"I would like to {my_goal}, so that I can {class_goal}!", end=" ")
 print("It feels so cool to be able to do this!!", end="\n")
+
+animal_names = ['Albatross', 'Baboon', 'Camel', 'Donkey', 'Eagle']
+for x in animal_names:
+    print(x)
+
+student_grades = [45, 57, 77, 85]
+for y in student_grades:
+    if y <= 60 and y > 50:
+        print('Average')
+    elif y > 60:
+        print('Excellent')
+    else:
+        print('Below Average')
